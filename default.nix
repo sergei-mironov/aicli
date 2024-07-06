5c9cf16dddaaf1657313afc964ff156bf68d3b09
Sergei Mironov <sergei.v.mironov@proton.me>
2024-07-06 14:07:58 +0400

    Update default.nix

--- default.nix
+++ default.nix
@@ -6,6 +6,17 @@ let
 
     inherit (pkgs) cmake fmt shaderc vulkan-headers vulkan-loader wayland pkg-config;
 
+    python = pkgs.python3;
+
+    python-dev = python.withPackages (
+      pp:  with pp; [
+        tqdm
+        requests
+        typing-extensions
+        importlib-resources
+        setuptools
+      ]
+    );
 
     shell = pkgs.mkShell {
       name = "shell";
@@ -17,14 +28,13 @@ let
         vulkan-loader
         wayland
         pkg-config
+        python-dev
       ];
     };
 
     gpt4all-backend = stdenv.mkDerivation (finalAttrs: {
-      pname = "gpt4all";
-      version = "2.7.5";
-
-      src = pkgs.gpt4all.src;
+      pname = "gpt4all-backend";
+      inherit (pkgs.gpt4all) version src;
 
       sourceRoot = "${finalAttrs.src.name}/gpt4all-backend";
 
@@ -41,6 +51,10 @@ let
         pkg-config
       ];
 
+      installPhase = ''
+        cp -rv . $out
+      '';
+
       cmakeFlags = [
         "-DKOMPUTE_OPT_USE_BUILT_IN_VULKAN_HEADER=OFF"
         "-DKOMPUTE_OPT_DISABLE_VULKAN_VERSION_CHECK=ON"
@@ -48,8 +62,71 @@ let
       ];
     });
 
+
+    gpt4all-bindings = (py: py.buildPythonPackage rec {
+      pname = "gpt4all-bindings";
+      inherit (pkgs.gpt4all) version src;
+
+      format = "setuptools";
+
+      dontUseCmakeConfigure = true;
+
+      preBuild = ''
+        cd gpt4all-backend
+        mkdir build
+        cp -rv ${gpt4all-backend} build
+        chmod -R +w build
+        cd ../gpt4all-bindings/python
+      '';
+
+      nativeBuildInputs = with py; [
+        pytest
+      ];
+
+      buildInputs = [
+        fmt
+        shaderc
+        vulkan-headers
+        vulkan-loader
+        wayland
+        pkg-config
+      ];
+
+      propagatedBuildInputs = with py; [
+        tqdm
+        requests
+        typing-extensions
+        importlib-resources
+      ];
+
+    });
+
+
+    # TODO: complete the package
+    gpt4all-cli = (py: py.buildPythonApplication rec {
+      pname = "gpt4all-cli";
+      inherit (pkgs.gpt4all) version src;
+
+      propagatedBuildInputs = [(gpt4all-bindings py.pkgs)];
+    });
+
+
+    python-gpt4all-bindings = python.withPackages (
+      pp:  with pp; [
+        (gpt4all-bindings pp)
+      ]
+    );
+
+    python-gpt4all-bindings-dev = python.withPackages (
+      pp:  with pp; [
+        ipython
+        (gpt4all-bindings pp)
+        typer
+      ]
+    );
+
     collection = rec {
-      inherit shell gpt4all-backend;
+      inherit shell gpt4all-backend python-gpt4all-bindings python-gpt4all-bindings-dev;
     };
   };
 
