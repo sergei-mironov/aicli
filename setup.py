524163ba3f096bbd7912d49737fcd7585e093013
Sergei Mironov <sergei.v.mironov@proton.me>
2024-07-25 16:05:43 +0400

    More systematic messages; Version 1.2.1

--- setup.py
+++ setup.py
@@ -33,7 +33,7 @@ with open("README.md", "r") as f:
 setup(
   name="gpt4all-cli",
   zip_safe=False, # https://mypy.readthedocs.io/en/latest/installed_packages.html
-  version="1.2.0",
+  version="1.2.1",
   package_dir={'':'python'},
   packages=find_packages(where='python'),
   long_description=long_description,
