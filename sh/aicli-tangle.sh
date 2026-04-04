0f1ba9fd5ed7e8ed60f4a9693461c50876edde04
Sergei Mironov <sergei.v.mironov@proton.me>
2026-04-04 18:02:21 +0400

    Aicli-tangle, add paste mode

--- sh/aicli-tangle.sh
+++ sh/aicli-tangle.sh
@@ -7,6 +7,7 @@ fi
 {
 echo '/set model replay on'
 $LITREPL --python-interpreter=- --sh-interpreter=- \
-  tangle --after-code=$'/ask\n' --after-result=$'/ans\n'
+  tangle --before-code=$'/paste on\n' --after-code=$'\n/paste off\n/ask\n' \
+         --before-result=$'/paste on\n' --after-result=$'\n/paste off\n/ans\n'
 echo '/set model replay off'
 }
