18ed6488179d41f7e794797878275a38a22c3091
Sergei Mironov <sergei.v.mironov@proton.me>
2026-04-04 17:49:12 +0400

    Update env

--- setup.py
+++ setup.py
@@ -60,6 +60,7 @@ setup(
   scripts=[
     join('.', 'python', 'aicli'),
     join('.', 'python', 'litrepl-aicli.py'),
+    join('.', 'sh', 'aicli-tangle.sh'),
   ],
   python_requires='>=3.12',
   author="Sergei Mironov",
--- sh/vimdev.sh
+++ sh/vimdev.sh
@@ -2,5 +2,12 @@
 
 # This is a small shell wrapper which adds LitREPL plugin from the current
 # repository to the VIM's runtime path.
-exec vim "$@"
+VIM=`which vim`
+PATH=\
+$AICLI_ROOT/python:$AICLI_ROOT/sh:\
+$(dirname `which git`):\
+$(dirname `which python`):\
+$(dirname `which litrepl`):\
+$(dirname $(readlink $(readlink `which ondir`)))\
+  exec $VIM "$@"
 
