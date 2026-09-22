86177b450a56fa34b0cb0cee9ba260c88945116b
Sergei Mironov <sergei.v.mironov@proton.me>
2026-09-22 15:15:15 +0400

    Update litepl-aicli-tangle

--- sh/litrepl-aicli-tangle.sh
+++ sh/litrepl-aicli-tangle.sh
@@ -4,6 +4,9 @@ if test -z "$LITREPL" ; then
   LITREPL=litrepl
 fi
 
+TW=''
+LOC=''
+DRYRUN=false
 while test "$#" -gt 0 ; do
     case "$1" in
         -h|--help) echo "TODO"; exit 1 ;;
@@ -11,10 +14,11 @@ while test "$#" -gt 0 ; do
         -s|--selection-paste) shift ;;
         -S|--selection-raw) shift ;;
         -f|--output-format) shift ;;
-        -w|--textwidth) shift ;;
+        -w|--textwidth) TW="--textwidth $2" ; shift; shift ;;
         -v|-d|--debug|--verbose) ;;
-        --dry-run) dry_run=1; ;;
+        --dry-run) DRYRUN=true; ;;
         --command) shift ;;
+        --loc) LOC=$2; shift; shift;;
         --) break ;;
         -*) echo "Unknown option: $1" >&2; exit 1 ;;
         *) FILES="$FILES $1" ;;
@@ -28,9 +32,9 @@ tee >(
 echo '/set model replay on'
 $LITREPL --python-interpreter=- --sh-interpreter=- \
   tangle --before-code=$'/paste on\n' --after-code=$'\n/paste off\n/ask\n' \
-         --before-result='' --after-result=$'/ans\n'
+         --before-result='' --after-result=$'/ans\n' $LOC
 echo '/set model replay off'
-} | $LITREPL eval-code ai "$@" >&2;
+} | $LITREPL eval-code ai $TW >&2;
 )
 
 
